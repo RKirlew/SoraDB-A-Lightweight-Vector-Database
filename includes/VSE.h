e01fbaa5a5617e5489380b4be8a5b2fd05d27fe5
R_Kirlew <kirlewraheem@gmail.com>
2025-04-11 21:24:15 -0500

    Update VSE.h

--- includes/VSE.h
+++ includes/VSE.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include <math.h>
+#include <sstream>
+#include "hnswlib/hnswlib.h"
+#include <fstream>
 class VSE{
 	public:
 		 std::unordered_map<std::string, std::vector<float>> vectorStorage;
@@ -13,7 +16,7 @@ class VSE{
 		 	// + 1e-8, avoid division by zero
 		 	float similarityScore;
 		 	float magnitude;
-		 	float sumOfVecs=0;
+		 	float sumOfVecs=0; 
 		 	float magnitudeSumA=0;
 		 	float magnitudeSumB=0;
 
@@ -30,8 +33,28 @@ class VSE{
     			return similarityScore;
 		 }
 		
-		bool insertVec(std::string id,std::vector<float> embedding){
-			
+		void insertVec(const std::string& id, const std::vector<float>& embedding) {
+    		vectorStorage[id] = embedding; // This will overwrite the existing vector if the key exists.
+		}
+		
+		
+		std::vector<float> getVec(const std::string& id) {
+        	return vectorStorage[id];
+    	}
+    	
+    	std::string readEmbeddingsFile(std::string soraFile){
+    		std::map<std::string, std::vector<float>> embeddings;
+    		std::string line;
+    		 // I want to store in a vector that is usable, a map with a string to a vector vec[0]->string vec[1]->vector
+    		 std::ifstream file_in(soraFile);
+   			 if (!file_in) {
+					std::cerr << "Could not open the file.\n";
+        			return "Could not open the file.\n";
+				}
+			while(std::getline(file_in,line)){
+				std::istringstream iss(line);
+			}
+    		return "The file data";
 		}
 
 };
